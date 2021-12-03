a65f6d3fb9df7398e6022818a759d71414c018bb
dcouch440 <dcouch440@gmail.com>
2021-12-02 22:45:54 -0800

    add createReducer

--- src/store/sidebar/reducer/index.js
+++ src/store/sidebar/reducer/index.js
@@ -4,6 +4,8 @@ import {
   UPDATE_VERSION,
 } from 'store/actions';
 
+import { createReducer } from 'store/utils';
+
 const initialState = {
   sidebarVersion: 'NAVIGATION',
   authType: 'LOGIN',
@@ -12,26 +14,22 @@ const initialState = {
   },
 };
 
-const reducer = (state = initialState, action) => {
-  const switchObj = {
-    [UPDATE_VERSION]: () => ({
-      ...state,
-      sidebarVersion: action.payload,
-    }),
-    [UPDATE_AUTH_TYPE]: () => ({
-      ...state,
-      authType: action.payload,
-    }),
-    [ADD_PASSWORD_ERROR]: () => ({
-      ...state,
-      errors: {
-        ...state.error,
-        passwordConfirm: action.payload,
-      },
-    }),
-  };
-
-  return (switchObj[action.type] || (() => state))();
-};
+const reducer = createReducer(initialState, (state, payload) => ({
+  [UPDATE_VERSION]: () => ({
+    ...state,
+    sidebarVersion: payload,
+  }),
+  [UPDATE_AUTH_TYPE]: () => ({
+    ...state,
+    authType: payload,
+  }),
+  [ADD_PASSWORD_ERROR]: () => ({
+    ...state,
+    errors: {
+      ...state.error,
+      passwordConfirm: payload,
+    },
+  }),
+}));
 
 export default reducer;
--- src/store/user/reducer/index.js
+++ src/store/user/reducer/index.js
@@ -1,4 +1,5 @@
 import { ADD_USER_TO_STORE } from 'store/actions';
+import { createReducer } from 'store/utils';
 
 const initialState = {
   id: null,
@@ -7,18 +8,14 @@ const initialState = {
   admin: false,
 };
 
-const reducer = (state = initialState, { payload, type }) => {
-  const switchObj = {
-    [ADD_USER_TO_STORE]: () => ({
-      ...state,
-      id: payload.username,
-      username: payload.username,
-      email: payload.email,
-      admin: payload.admin,
-    }),
-  };
-
-  return (switchObj[type] || (() => state))();
-};
+const reducer = createReducer(initialState, (state, payload) => ({
+  [ADD_USER_TO_STORE]: () => ({
+    ...state,
+    id: payload.username,
+    username: payload.username,
+    email: payload.email,
+    admin: payload.admin,
+  }),
+}));
 
 export default reducer;
