252c355c8880727d8bc79b66b78d96b71b0986fb
dcouch440 <dcouch440@gmail.com>
2021-12-04 00:09:21 -0800

    add suspense

--- src/components/Sidebar/Nav/index.js
+++ src/components/Sidebar/Nav/index.js
@@ -1,7 +1,7 @@
+import React, { Suspense, lazy } from 'react';
+
 import { Box } from '@mui/system';
 import Header from '../Header';
-import NavigationLink from './NavigationLink';
-import React from 'react';
 import ScrollContainer from 'components/ScrollContainer';
 
 const styles = () => ({
@@ -10,6 +10,8 @@ const styles = () => ({
   },
 });
 
+const LazyNavigationLink = lazy(() => import('./NavigationLink'));
+
 export default function Nav() {
   const sx = styles();
 
@@ -17,51 +19,61 @@ export default function Nav() {
     <Box sx={sx.root}>
       <ScrollContainer hideScrollBar style={sx.scrollbar}>
         <Header title="What Doing?" />
-        <NavigationLink
-          title="Enchants"
-          description="Description Text; Description Text; Description Text; Description Text;"
-          path="/"
-          images={[
-            'https://res.cloudinary.com/dbyretay5/image/upload/v1620686977/category/art_mpxdif.webp',
-            'https://res.cloudinary.com/dbyretay5/image/upload/v1620686973/category/pexels-mart-production-7679889_oqrtec.webp',
-          ]}
-        />
-        <NavigationLink
-          title="Likes"
-          description="Description Text; Description Text; Description Text; Description Text;"
-          path="/"
-          images={[
-            'https://res.cloudinary.com/dbyretay5/image/upload/v1620686973/category/pexels-pixabay-221327_dhphth.webp',
-            'https://res.cloudinary.com/dbyretay5/image/upload/v1620686977/category/art_mpxdif.webp',
-          ]}
-        />
-        <NavigationLink
-          title="Post"
-          description="Description Text; Description Text; Description Text; Description Text;"
-          path="/"
-          images={[
-            'https://res.cloudinary.com/dbyretay5/image/upload/v1620686977/category/art_mpxdif.webp',
-            'https://res.cloudinary.com/dbyretay5/image/upload/v1620686973/category/pexels-pixabay-221327_dhphth.webp',
-          ]}
-        />
-        <NavigationLink
-          title="Barter"
-          description="Description Text; Description Text; Description Text; Description Text;"
-          path="/"
-          images={[
-            'https://res.cloudinary.com/dbyretay5/image/upload/v1620686977/category/art_mpxdif.webp',
-            'https://res.cloudinary.com/dbyretay5/image/upload/v1620686973/category/pexels-pixabay-221327_dhphth.webp',
-          ]}
-        />
-        <NavigationLink
-          title="Go Live"
-          description="Description Text; Description Text; Description Text; Description Text;"
-          path="/"
-          images={[
-            'https://res.cloudinary.com/dbyretay5/image/upload/v1620686977/category/art_mpxdif.webp',
-            'https://res.cloudinary.com/dbyretay5/image/upload/v1620686973/category/pexels-pixabay-221327_dhphth.webp',
-          ]}
-        />
+        <Suspense fallback={<div />}>
+          <LazyNavigationLink
+            title="Enchants"
+            description="Description Text; Description Text; Description Text; Description Text;"
+            path="/"
+            images={[
+              'https://res.cloudinary.com/dbyretay5/image/upload/v1620686977/category/art_mpxdif.webp',
+              'https://res.cloudinary.com/dbyretay5/image/upload/v1620686973/category/pexels-mart-production-7679889_oqrtec.webp',
+            ]}
+          />
+        </Suspense>
+        <Suspense fallback={<div />}>
+          <LazyNavigationLink
+            title="Likes"
+            description="Description Text; Description Text; Description Text; Description Text;"
+            path="/"
+            images={[
+              'https://res.cloudinary.com/dbyretay5/image/upload/v1620686973/category/pexels-pixabay-221327_dhphth.webp',
+              'https://res.cloudinary.com/dbyretay5/image/upload/v1620686977/category/art_mpxdif.webp',
+            ]}
+          />
+        </Suspense>
+        <Suspense fallback={<div />}>
+          <LazyNavigationLink
+            title="Post"
+            description="Description Text; Description Text; Description Text; Description Text;"
+            path="/"
+            images={[
+              'https://res.cloudinary.com/dbyretay5/image/upload/v1620686977/category/art_mpxdif.webp',
+              'https://res.cloudinary.com/dbyretay5/image/upload/v1620686973/category/pexels-pixabay-221327_dhphth.webp',
+            ]}
+          />
+        </Suspense>
+        <Suspense fallback={<div />}>
+          <LazyNavigationLink
+            title="Barter"
+            description="Description Text; Description Text; Description Text; Description Text;"
+            path="/"
+            images={[
+              'https://res.cloudinary.com/dbyretay5/image/upload/v1620686977/category/art_mpxdif.webp',
+              'https://res.cloudinary.com/dbyretay5/image/upload/v1620686973/category/pexels-pixabay-221327_dhphth.webp',
+            ]}
+          />
+        </Suspense>
+        <Suspense fallback={<div />}>
+          <LazyNavigationLink
+            title="Go Live"
+            description="Description Text; Description Text; Description Text; Description Text;"
+            path="/"
+            images={[
+              'https://res.cloudinary.com/dbyretay5/image/upload/v1620686977/category/art_mpxdif.webp',
+              'https://res.cloudinary.com/dbyretay5/image/upload/v1620686973/category/pexels-pixabay-221327_dhphth.webp',
+            ]}
+          />
+        </Suspense>
       </ScrollContainer>
     </Box>
   );
