6629dbb5a4a74c0cbf4233cb6f1430bce4f7a735
dcouch440 <dcouch440@gmail.com>
2022-01-01 01:36:32 -0800

    add return cluase

--- src/store/enchant/thunkCreators.ts
+++ src/store/enchant/thunkCreators.ts
@@ -3,32 +3,35 @@ import { ThunkCreators } from 'store/types';
 import axios from 'axios';
 import { enchantsRetrieved } from './actionCreators';
 
-export const getEnchants =
-  (limit = 15): ThunkCreators =>
-  async (dispatch, getState) => {
-    try {
-      const { lastSeen, searchTags } = getState().enchant;
-      const queryDate = lastSeen ? `&lastseen=${lastSeen}` : '';
-      const queryTags = searchTags.length
-        ? `&tags=${searchTags.toString()}`
-        : '';
+export const getEnchants = (): ThunkCreators => async (dispatch, getState) => {
+  try {
+    const { lastSeen, searchTags, enchants } = getState().enchant;
+    const queryDate = lastSeen ? `&lastseen=${lastSeen}` : '';
+    const queryTags = searchTags.length ? `&tags=${searchTags.toString()}` : '';
+    const limit = 15;
+    // if a previous request was made and the results where less
+    // than the maximum value, then the query did not find enough
+    // the thunk should return to prevent the api getting a second
+    // request from the intersection observer
+    // as the request will result it nothing.
+    if (enchants.length < limit && lastSeen !== null) return;
 
-      const { data } = await axios.get<IEnchant[]>(
-        `/enchants?limit=${limit}${queryDate}${queryTags}`
-      );
+    const { data } = await axios.get<IEnchant[]>(
+      `/enchants?limit=${limit}${queryDate}${queryTags}`
+    );
 
-      if (!data?.length || !data) throw new Error('No antiques found');
+    if (!data?.length || !data) throw new Error('No antiques found');
 
-      const newLastSeen = data[data.length - 1].id;
+    const newLastSeen = data[data.length - 1].id;
 
-      dispatch(
-        enchantsRetrieved({
-          enchants: data,
-          lastSeen: newLastSeen,
-        })
-      );
-    } catch (err) {
-      //TODO: handle error, snackbar?
-      console.error(err);
-    }
-  };
+    dispatch(
+      enchantsRetrieved({
+        enchants: data,
+        lastSeen: newLastSeen,
+      })
+    );
+  } catch (err) {
+    //TODO: handle error, snackbar?
+    console.error(err);
+  }
+};
