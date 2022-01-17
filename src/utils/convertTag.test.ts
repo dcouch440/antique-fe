ec72861dbb52cbb602d5e37218369f1f69d811b0
dcouch440 <dcouch440@gmail.com>
2022-01-17 13:00:09 -0800

    fix typo

--- src/utils/convertTag.test.ts
+++ src/utils/convertTag.test.ts
@@ -1,6 +1,6 @@
 import convertTag from './convertTag';
 
-describe('converTag', () => {
+describe('convertTag', () => {
   it('converts spaces, removes special chars and adds a hash tag.', () => {
     const t = convertTag('&AB       !!@#$%^&*()_+                  CD***');
     expect(t).toEqual('#abcd');
